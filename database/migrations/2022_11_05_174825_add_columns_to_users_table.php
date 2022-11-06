6a726126b3906f0fdbf621c100feba28f366ad74
mario.mamdouh <mario.mamdouh@kickstartinteractive.net>
2022-11-06 14:00:40 +0200

    add user module and seed users from json

---
+++ database/migrations/2022_11_05_174825_add_columns_to_users_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(): void
+    {
+        Schema::table('users', static function (Blueprint $table) {
+            $table->uuid('id')->change();
+            $table->float('balance')->default(0.0);
+            $table->string('currency', 3);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down(): void
+    {
+        Schema::table('users', static function (Blueprint $table) {
+            $table->dropColumn('balance');
+            $table->dropColumn('currency');
+        });
+    }
+};
--- routes/auth.php
+++ routes/auth.php
@@ -6,3 +6,9 @@ Route::group(['prefix' => 'auth'] , static function (){
     Route::post('register', [App\Http\Controllers\Api\AuthController::class, 'registerUser']);
     Route::post('login', [App\Http\Controllers\Api\AuthController::class, 'loginUser']);
 });
+
+Route::group(['middleware' => 'auth:sanctum'] , function (){
+   Route::get('me' , static function (\Illuminate\Http\Request $request){
+       return $request->user();
+   });
+});
